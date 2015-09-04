d152a6261f37b2157fc90099cd2b46acecfb888b
ds007007 <ds007007@yandex.ru>
2015-09-04 15:50:36 +0300

    Header directive

---
+++ directive/header/header.js
@@ -0,0 +1,10 @@
+angular.module('cloudStream').directive('appHeader', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'directive/header/header.html',
+		link: function(scope, element, attrs, fn) {
+
+
+		}
+	};
+});
