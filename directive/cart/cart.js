4af6ffb109bae552d72449a45a74748ac7493c4a
ds007007 <ds007007@yandex.ru>
2015-09-07 12:04:25 +0300

    Come cart fixes

--- directive/cart/cart.js
+++ directive/cart/cart.js
@@ -12,21 +12,20 @@ angular.module('cloudStream').directive('appCart', function() {
 
       (function(){
       var a = document.querySelector('#cart'), b = null, P = 0;
-      window.addEventListener('scroll', Ascroll, false);
-      document.body.addEventListener('scroll', Ascroll, false);
+
       function Ascroll() {
         if (b == null) {
           var Sa = getComputedStyle(a, ''), s = '';
           for (var i = 0; i < Sa.length; i++) {
-            if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
-              s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
-            }
+            if (Sa[i].indexOf('overflow') === 0 || Sa[i].indexOf('padding') === 0 || Sa[i].indexOf('border') === 0 || Sa[i].indexOf('outline') === 0 || Sa[i].indexOf('box-shadow') === 0 || Sa[i].indexOf('background') === 0){
+                s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; ';
+              }
           }
           b = document.createElement('div');
           b.style.cssText = s + ' width: ' + a.offsetWidth + 'px;';
           a.insertBefore(b, a.firstChild);
           var l = a.childNodes.length;
-          for (var i = 1; i < l; i++) {
+          for (i = 1; i < l; i++) {
             b.appendChild(a.childNodes[1]);
           }
           a.style.height = b.getBoundingClientRect().height + 'px';
@@ -49,10 +48,14 @@ angular.module('cloudStream').directive('appCart', function() {
           b.style.top = '';
         }
         window.addEventListener('resize', function() {
-          a.children[0].style.width = getComputedStyle(a, '').width
+          a.children[0].style.width = getComputedStyle(a, '').width;
         }, false);
       }
-      })()
+
+      window.addEventListener('scroll', Ascroll, false);
+      document.body.addEventListener('scroll', Ascroll, false);
+
+      })();
 
 		}
 
